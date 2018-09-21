b06080769d303e09a217c71300c0d090b6ca51a9
gcaaa31928 <gcaaa31928@gmail.com>
2018-09-22 00:14:54 +0800

    feat: prototyping

--- src/vnode.js
+++ src/vnode.js
@@ -1,13 +1,36 @@
 const VNodeUUID = Symbol('VNodeUUID');
 class VNode {
-	constructor(tag, props, children) {
+	constructor(tag, props, key, children) {
 		this.$uuid = VNodeUUID;
 		this._tag = tag;
 		this._props = props;
-		this._childrent = children;
+		this._key = key;
+		this._children = children || [];
 	}
-	static isSameNode(VNode leftNode, VNode rightNode) {
+	render() {
+		var element = document.createElement(this._tag);
+
+		var props = this._props;
+		for (var propKey in this._props) {
+			prop = this._props[propKey];
+			element.setAttribute(propKey, prop);
+		}
+		if (Array.isArray(children)) {
+			children.forEach(function(child) {
+				var childEl;
+				if (child instanceof VNode) {
+					childEl = child.render();
+				} else {
+					childEl = document.createTextNode(child);
+				}
+				element.appendChild(child);
+			});
+		}
+		return element;
+	}
+	isSameNode(theNode) {
+		return this._key === theNode._key && this._tag === theNode._tag;
 	}
 }
 
-export VNode;
+export default VNode;
